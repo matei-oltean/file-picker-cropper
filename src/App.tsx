73cf4ad9d0887be031619484a25d9edc8e311b5e
Matei Oltean <matei.oltean@gmail.com>
2022-10-01 22:34:30 +0200

    It works

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,39 @@
-import React, { ChangeEventHandler, useRef } from "react";
-import AvatarImageCropper from 'react-avatar-image-cropper';
-import "./App.css";
+import React from "react";
+import AvatarImageCropper from "react-avatar-image-cropper";
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
 
 function App() {
-  const [images, setImages] = React.useState<string[]>([]);
+  const [images, setImages] = React.useState<File[]>([]);
   const apply = (file) => {
-    // handle the blob file you want
-    // such as get the image src
-    var src = URL.createObjectURL(file);
-    setImages([...images, src]);
-  }
-  // const fileInput = useRef(null)
-  // const onChangeHandler: ChangeEventHandler<HTMLInputElement> = (event) => {
-  //   const newImages = [...images, event.target.files[0]];
-  //   setImages(newImages);
-  // };
+    setImages([...images, file]);
+  };
+
+  const zipFiles = () => {
+    var zip = new JSZip();
+    var img = zip.folder("images");
+    for (let i = 0; i < images.length; ++i) {
+      img.file(`img-${i}.png`, images[i]);
+    }
+    img
+      .generateAsync({ type: "blob" })
+      .then((content) => saveAs(content, "images"));
+    setImages([]);
+  };
+
   return (
     <div>
       <div>
-        {/* <button
-          className="upload-btn"
-          onClick={() => fileInput.current.click()}
-        >
-          Choose PNG File
-        </button> */}
-        {/* <input
-          type="file"
-          id="image_uploads"
-          ref={fileInput}
-          onChange={onChangeHandler}
-          accept=".png"
-          style={{ display: "none" }}
-        /> */}
-        <div style={{width:'512px',height:'512px',margin:'auto'}}>
-        <AvatarImageCropper apply={apply} />
+        <div style={{ width: "512px", height: "512px", margin: "auto" }}>
+          <AvatarImageCropper apply={apply} />
         </div>
       </div>
       <div>
-        {images.map((src: string) => (
-          <img src={src} height={512} />
+        {images.map((file: File) => (
+          <img src={URL.createObjectURL(file)} height={128} />
         ))}
       </div>
-      <div>
-        <button>Submit</button>
-      </div>
+      <button onClick={zipFiles}>Download</button>
     </div>
   );
 }
